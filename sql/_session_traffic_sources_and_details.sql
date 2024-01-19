ba51c21689b12b32c381c5c1ec4a0570ae651d08
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-01-19 11:26:05 -0500

    updated query with count of pageviews

--- sql/_session_traffic_sources_and_details.sql
+++ sql/_session_traffic_sources_and_details.sql
@@ -19,16 +19,36 @@ session_start_dates AS (
   SELECT
     user_pseudo_id,
     ga_session_id,
-    MIN(event_timestamp) as min_session_event_timestamp
+    MIN(event_timestamp) as min_session_event_timestamp,
+    MAX(event_timestamp) as max_session_event_timestamp
   FROM hit_traffic_sources
-  GROUP BY 
+  GROUP BY
     1,2
+),
+
+pageviews AS (
+  SELECT
+    user_pseudo_id,
+    ga_session_id,
+    COUNT(*) as pageviews
+  FROM (
+    SELECT 
+      user_pseudo_id,
+      (SELECT value.int_value FROM UNNEST(event_params) as ep WHERE key = 'ga_session_id') as ga_session_id,
+      event_name
+    FROM `key-utility-407314.eh_ga4_obfuscated_sample_ecommerce.eh_ga4_obfuscated_filtered`
+    WHERE event_name = 'page_view'   
+  ) as pv
+  GROUP BY 
+    pv.user_pseudo_id,
+    pv.ga_session_id
 ),
 
 session_traffic_sources AS (
   SELECT
     hts.user_pseudo_id,
     hts.ga_session_id,
+    ssd.max_session_event_timestamp,
     ssd.min_session_event_timestamp,
 
     ----------------------------------
@@ -67,11 +87,16 @@ session_traffic_sources AS (
       RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
     ) as session_traffic_campaign,
 
+    -- measures
+    pv.pageviews
+    
   FROM hit_traffic_sources as hts
     LEFT JOIN session_start_dates as ssd
-    ON hts.user_pseudo_id = ssd.user_pseudo_id
-    AND hts.ga_session_id = ssd.ga_session_id 
-
+      ON hts.user_pseudo_id = ssd.user_pseudo_id
+      AND hts.ga_session_id = ssd.ga_session_id
+    LEFT JOIN pageviews as pv
+      ON hts.user_pseudo_id = pv.user_pseudo_id
+      AND pv.user_pseudo_id = pv.user_pseudo_id
   ORDER BY hts.user_pseudo_id, hts.event_timestamp
 )
 
@@ -80,12 +105,18 @@ SELECT DISTINCT
   user_pseudo_id,
   ga_session_id,
   TIMESTAMP_MICROS(min_session_event_timestamp) as min_session_event_timestamp,
+  TIMESTAMP_MICROS(max_session_event_timestamp) as max_session_event_timestamp,
+  TIMESTAMP_DIFF(TIMESTAMP_MICROS(max_session_event_timestamp), TIMESTAMP_MICROS(min_session_event_timestamp), SECOND) AS session_duration_seconds,
   ga_session_number,
   session_traffic_source,
   session_traffic_medium,
-  session_traffic_campaign
+  session_traffic_campaign,
+  SUM(pageviews) as pageviews, 
 FROM session_traffic_sources
+
 --WHERE user_pseudo_id = '2221352.0772999791'
+
 GROUP BY
-  1,2,3,4,5,6,7
+  1,2,3,4,5,6,7,8,9
+
 ORDER BY user_pseudo_id, min_session_event_timestamp
