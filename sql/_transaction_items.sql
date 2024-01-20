975ba1449adc6f7c7ebc36242dc0bd70b3297703
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-01-20 13:30:56 -0500

    added metrics

--- sql/_transaction_items.sql
+++ sql/_transaction_items.sql
@@ -1,11 +1,12 @@
 WITH results as  (
   SELECT
     event_timestamp,
-    event_name,
     user_pseudo_id,
     ecommerce.transaction_id as transaction_id,
-    ecommerce.total_item_quantity as total_item_quantity,
-    item.item_id
+    item.item_id,
+    item.price,
+    item.quantity,
+    IFNULL(item.price,0) * IFNULL(item.quantity,0) as revenue
   FROM
     `key-utility-407314.eh_ga4_obfuscated_sample_ecommerce.eh_ga4_obfuscated_filtered`,
     UNNEST(items) AS item
@@ -20,10 +21,13 @@ SELECT
   transaction_id,
   item_id,
   event_timestamp,
-  SUM(total_item_quantity) as total_item_quantity
-  SUM(revenue)
+  AVG(price) as price,
+  SUM(quantity) as quantity,
+  SUM(revenue) as revenue
 FROM results
 GROUP BY
   transaction_id,
   item_id,
   event_timestamp
+ORDER BY transaction_id, item_id
+
