7bbc9e2b92420b6817ecd1dbb3ea85d63ec8ae14
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-02-02 15:20:36 -0500

    adjusted where sessions and pageviews are pulled from

--- sql/__prod_users_details.sql
+++ sql/__prod_users_details.sql
@@ -6,13 +6,30 @@ WITH users as (
     u.user_traffic_name,
     --u.geo,
     u.min_user_event_timestamp,
-    u.max_user_event_timestamp,
-    SUM(u.count_of_sessions) as total_sessions,
-    SUM(u.pageviews) as total_pageviews
+    u.max_user_event_timestamp
   FROM 
     `key-utility-407314.eh_ga4_stg._users` as u
   GROUP BY
     1,2,3,4,5,6
+),
+
+sessions as (
+  SELECT 
+    s.user_pseudo_id,
+    COUNT(DISTINCT s.ga_session_id) as total_sessions
+  FROM
+    `key-utility-407314.eh_ga4_stg._sessions` as s
+  GROUP BY s.user_pseudo_id
+),
+
+pageviews as (
+  SELECT 
+    p.user_pseudo_id,
+    COUNT(DISTINCT p.event_timestamp) as total_pageviews
+  FROM
+    `key-utility-407314.eh_ga4_obfuscated_sample_ecommerce.eh_ga4_obfuscated_filtered` as p
+  WHERE p.event_name = 'page_view'
+  GROUP BY p.user_pseudo_id
 ),
 
 transactions as (
@@ -37,8 +54,8 @@ SELECT
   --u.geo,
   u.min_user_event_timestamp,
   u.max_user_event_timestamp,
-  SUM(u.total_sessions) as total_sessions,
-  SUM(u.total_pageviews) as total_pageviews,
+  SUM(s.total_sessions) as total_sessions,
+  SUM(p.total_pageviews) as total_pageviews,
   SUM(t.total_transactions) as total_transactions,
   SUM(t.total_item_quantity) as total_items_purchased,
   SUM(t.total_revenue) as total_revenue
@@ -46,6 +63,10 @@ FROM users as u
 
 LEFT JOIN transactions as t
   ON u.user_pseudo_id = t.user_pseudo_id
+LEFT JOIN sessions as s
+  ON u.user_pseudo_id = s.user_pseudo_id
+LEFT JOIN pageviews as p
+  ON u.user_pseudo_id = p.user_pseudo_id
 
 GROUP BY
   1,2,3,4,5,6
