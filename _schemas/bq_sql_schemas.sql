b4742d700f26592cdf39b16854f4db10d63ea6cd
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-02-13 13:36:24 -0500

    added __prod_ tables to schemas.sql

--- _schemas/bq_sql_schemas.sql
+++ _schemas/bq_sql_schemas.sql
@@ -1,47 +1,98 @@
 -- Transaction Table
+CREATE TABLE __user_details (
+    user_pseudo_id CHAR NOT NULL,
+    --traffic_source_id CHAR NOT NULL,
+    user_traffic_source CHAR,
+    user_traffic_medium CHAR,
+    user_Traffic_campaign CHAR,
+    min_user_event_timestamp DATETIME,
+    max_user_event_timestamp DATETIME,
+    total_sessions INT,
+    total_pageviews INT,
+    total_transactions INT,
+    total_items_purchased INT,
+    total_revenue FLOAT
+)
+
+CREATE TABLE __users_session_details (
+    user_pseudo_id CHAR NOT NULL,
+    ga_session_id CHAR NOT NULL,
+    --traffic_source_id CHAR NOT NULL,
+    user_traffic_source CHAR,
+    user_traffic_medium CHAR,
+    user_traffic_campaign CHAR,
+    min_user_event_timestamp DATETIME,
+    max_user_event_timestamp DATETIME,
+    session_traffic_campaign CHAR,
+    session_traffic_medium CHAR,
+    session_traffic_source CHAR,
+    min_session_event_timestamp DATETIME,
+    max_session_event_timestamp DATETIME,
+    total_sessions INT,
+    total_pageviews INT,
+    total_transactions INT,
+    total_items_purchased INT,
+    total_revenue FLOAT    
+)
+
+CREATE TABLE __transaction_details (
+    user_pseudo_id CHAR NOT NULL,
+    ga_session_id CHAR NOT NULL,
+    --traffic_source_id CHAR NOT NULL,
+    session_traffic_campaign CHAR,
+    session_traffic_medium CHAR,
+    session_traffic_source CHAR,
+    min_session_event_timestamp DATETIME,
+    max_session_event_timestamp DATETIME,
+    transaction_id CHAR,
+    item_id CHAR,
+    total_quantity CHAR,
+    total_revenue CHAR
+)
+
 CREATE TABLE Transaction (
-    Transaction_ID STRING NOT NULL,
-    User_ID STRING,
-    Session_ID STRING,
+    Transaction_ID CHAR NOT NULL,
+    User_ID CHAR,
+    Session_ID CHAR,
     Date_and_Time DATETIME NOT NULL,
     Total_Transaction_Value FLOAT
 );
 
 -- Item Table
 CREATE TABLE Item (
-    Item_ID STRING NOT NULL,
-    Item_Category STRING,
+    Item_ID CHAR NOT NULL,
+    Item_Category CHAR,
     Item_Price FLOAT
 );
 
 -- Transaction Item Table
 CREATE TABLE Transaction_Item (
-    Transaction_Item_ID STRING NOT NULL,
-    Transaction_ID STRING NOT NULL,
-    Item_ID STRING NOT NULL,
+    Transaction_Item_ID CHAR NOT NULL,
+    Transaction_ID CHAR NOT NULL,
+    Item_ID CHAR NOT NULL,
     Quantity INT
 );
 
 -- User Table
 CREATE TABLE User (
-    User_ID STRING NOT NULL,
+    User_ID CHAR NOT NULL,
     User_First_Visit_Date DATE,
-    Geographic_Location STRING,
-    First_Visit_Traffic_Source STRING
+    Geographic_Location CHAR,
+    First_Visit_Traffic_Source CHAR
 );
 
 -- Session Table
 CREATE TABLE Session (
-    Session_ID STRING NOT NULL,
-    User_ID STRING,
+    Session_ID CHAR NOT NULL,
+    User_ID CHAR,
     Session_Start_Time DATETIME,
     Session_Duration INT,
-    Session_Traffic_Source_ID STRING
+    Session_Traffic_Source_ID CHAR
 );
 
 -- Traffic Source Table
 CREATE TABLE Traffic_Source (
-    Traffic_Source_ID STRING NOT NULL,
-    Source_Type STRING,
-    Source_Name STRING
+    Traffic_Source_ID CHAR NOT NULL,
+    Source_Type CHAR,
+    Source_Name CHAR
 );
